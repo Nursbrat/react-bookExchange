52f48a41e086cc3a1313dbf861e45230812c7d39
Ttoorne <karakozhaev.bektur@gmail.com>
2023-08-17 01:35:41 +0600

    auth fixed and routes added

---
+++ src/constants/routes.js
@@ -0,0 +1,29 @@
+import Account from "../pages/Account/Account";
+import Addbook from "../pages/Addbook/Addbook";
+import Addbook2 from "../pages/Addbook2/Addbook2";
+import Auth from "../pages/Auth/Auth";
+import LoginForm from "../pages/Auth/components/LoginForm/LoginForm";
+import SignUpForm from "../pages/Auth/components/SignUpForm/SignUpForm";
+import BookInfo from "../pages/BookInfo/BookInfo";
+import Home from "../pages/Home/Home";
+import MainPage from "../pages/MainPage/MainPage";
+import MyBooks from "../pages/MyBooks/MyBooks";
+import Profile from "../pages/Profile/Profile";
+import Requests from "../pages/Requests/Requests";
+// import SubMainPage from "../pages/SubMainPage/SubMainPage";
+
+export const ROUTES = [
+  { link: "/", component: MainPage, id: 1 },
+  { link: "/home", component: Home, id: 2 },
+  { link: "/auth", component: Auth, id: 3 },
+  { link: "/login", component: LoginForm, id: 4 },
+  { link: "/sign-up", component: SignUpForm, id: 5 },
+  { link: "/my-books", component: MyBooks, id: 6 },
+  { link: "/requests", component: Requests, id: 7 },
+  { link: "/account", component: Account, id: 8 },
+  { link: "/add-book", component: Addbook, id: 9 },
+  { link: "/add-book-two", component: Addbook2, id: 10 },
+  { link: "/book-info", component: BookInfo, id: 11 },
+  //   { link: "/submain-page", component: SubMainPage, id: 12 },
+  { link: "/profile", component: Profile, id: 13 },
+];
--- src/pages/SubMainPage/components/Books/Books.jsx
+++ src/pages/SubMainPage/components/Books/Books.jsx
@@ -1,38 +1,39 @@
-import '../Books/Books.scss';
+// import "../Books/Books.scss";
 
-import images from '../../../../constants/images';
-import { useSelector } from '@reduxjs/toolkit'
+// import images from "../../../../constants/images";
+// import { useSelector } from "@reduxjs/toolkit";
 
+// const Books = () => {
+//   // const dispatch = useDispatch()
+//   const isDarkMode = useSelector((state) => state.darkMode);
 
-const Books = () => {
+//   const books = [
+//     { img: images.book, title: "Book Title 1" },
+//     { img: images.book, title: "Book Title 2" },
+//   ];
+//   return (
+//     <div className="books">
+//       <div className="container">
+//         <div
+//           className={`'books__genere--text'${isDarkMode ? "dark-mode" : ""}`}
+//         >
+//           Поэзия
+//         </div>
+//         <div className="books__content">
+//           <div className="books__items">
+//             {books.map((book, index) => (
+//               <div key={index} className="books__item">
+//                 <img src={book.img} alt="" />
+//                 <p className={`'${isDarkMode ? "dark-mode" : ""}`}>
+//                   {book.title}
+//                 </p>
+//               </div>
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-    // const dispatch = useDispatch()
-    const isDarkMode = useSelector(state => state.darkMode)
-
-    const books = [
-        { img: images.book, title: 'Book Title 1' },
-        { img: images.book, title: 'Book Title 2' },
-
-    ]
-    return (
-
-        <div className="books">
-            <div className="container">
-                <div className={`'books__genere--text'${isDarkMode ? 'dark-mode' : ''}`}>Поэзия</div>
-                <div className="books__content">
-                    <div className="books__items">
-                        {books.map((book, index) => (
-                            <div key={index} className="books__item">
-                                <img src={book.img} alt="" />
-                                <p className={`'${isDarkMode ? 'dark-mode' : ''}`}>{book.title}</p>
-
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Books
+// export default Books;
