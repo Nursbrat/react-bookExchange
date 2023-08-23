75f84cbf2d4857523b0ae1a11193f60fe1316b93
nurs <nurstilek@gmail.com>
2023-08-23 13:24:36 +0600

    merge

--- src/app/store.js
+++ src/app/store.js
@@ -1,20 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD
-import { darkModeToggleReducer } from "../features/themeToggle/themeToggleSlice";
+
 import { booksApi } from "./services/booksApi";
-=======
 import themeToggleSliceReducer from "../features/themeToggle/themeToggleSlice";
->>>>>>> 2f8efae5ab5314f174151a577eeb2a5a7a5b6523
-
-
 const store = configureStore({
   reducer: {
-<<<<<<< HEAD
-    darkMode: darkModeToggleReducer,
     [booksApi.reducerPath]: booksApi.reducer,
-=======
     theme: themeToggleSliceReducer,
->>>>>>> 2f8efae5ab5314f174151a577eeb2a5a7a5b6523
+
   },
 });
 
